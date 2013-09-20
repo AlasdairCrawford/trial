80f4ec9af2799de0db75d8b18097144c0cbab55b
Viswanathan1 <vilayanur.viswanathan@pnnl.gov>
2013-09-20 11:57:54 -0700

    new branch propsed off vo

--- githubtrialsept20/helloworld.cpp
+++ githubtrialsept20/helloworld.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
 	int x;
-	cout << "Hello World and Universe"; //let us be inclusive of all galaxies
+	cout << "Hello World and Universe"; //let us be inclusive of all galaxies. will send branch info Pleas place thi in new branch off V0
 	cin >> x;
 }
